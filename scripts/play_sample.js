433eba53d398d4466a7d83b2f9fd5696e7050c8c
Vladimir Masko <soupdawgie@gmail.com>
2016-05-17 21:31:17 +0300

    Refactor playSample function, fix delay issue

--- scripts/play_sample.js
+++ scripts/play_sample.js
@@ -1,28 +1,23 @@
+var keys = { 65: 'kick', 83: 'clap', 68: 'hat' }
+
 $(document).keydown(function(event) {
   var key = event.which;
-  if(key == 65) {
-    $('#kick')[0].play();
-    $('.kick').css('background-color', '#751aff')
+  function playSample(k) {
+    $('#' + keys[k]).clone()[0].play();
+    $('.' + keys[k]).css('background-color', '#751aff');
   }
-  else if(key == 83) {
-    $('#clap')[0].play();
-    $('.clap').css('background-color', '#751aff')
-  }
-  else if(key == 68) {
-    $('#hat')[0].play();
-    $('.hat').css('background-color', '#751aff')
-  }
+  playSample(key);
 });
 
 $(document).keyup(function(event) {
   var key = event.which;
   if(key == 65) {
-    $('.kick').css('background-color', '#ff1a1a')
+    $('.kick').css('background-color', '#ff1a1a');
   }
   else if(key == 83) {
-    $('.clap').css('background-color', '#ff4d4d')
+    $('.clap').css('background-color', '#ff4d4d');
   }
   else if(key == 68) {
-    $('.hat').css('background-color', '#ff8080')
+    $('.hat').css('background-color', '#ff8080');
   }
 });
